1bbcd7f7538db30dba4758c208fd6a42233397f3
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-05-11 16:54:06 +0300

    adding changeOwner function

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ import {
   mkfile, mkdir, isDirectory, isFile, map, getChildren, getName, getMeta
 } from '@hexlet/immutable-fs-trees';
 
-const tree = mkdir('/', [
-  mkfile('one'),
-  mkfile('two'),
-  mkdir('three'),
-]);
+const originalTree = mkdir('/', [
+  mkfile('one', { owner: 'Vasya' }),
+  mkfile('two', { owner: 'Kolya' }),
+  mkdir('three', [], { owner: 'Kirill' }),
+], { owner: 'Vasya' });
 
-const children = getChildren(tree);
-const newChildren = children.filter(isDirectory);
-console.log(newChildren);
-const newMeta =  _.cloneDeep(getMeta(tree));
-const tree2 = mkdir(getName(tree), newChildren, newMeta);
-console.log(tree2);
+const changeOwner = (tree, newOwner) => {
+  const newName = getName(tree);
+  const newMeta = _.cloneDeep(getMeta(tree));
+  newMeta.owner = newOwner;
+  if (isFile(tree)) {
+    return mkfile(newName, newMeta);
+  }
+  const children = getChildren(tree);
+  const newChildren = children.map((child) => changeOwner(child, newOwner));
+  const newTree = mkdir(newName, newChildren, newMeta);
+  return newTree;
+}
 
+console.log(originalTree.children);
+
+const newTree = changeOwner(originalTree, 'Slava');
+
+console.log(newTree.children);
+console.log(originalTree.children);
