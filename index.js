a92f67fd688eac91ff7d0e4b132beea6c27ac941
Viacheslav Kuznetsov <via.kuznetsov@gmail.com>
2021-05-12 19:23:28 +0200

    adding getSubdirectoriesInfo

--- index.js
+++ index.js
@@ -6,36 +6,41 @@ import {
 
 const tree = mkdir('/', [
   mkdir('etc', [
-    mkfile('bashrc'),
-    mkfile('consul.cfg'),
+    mkdir('apache'),
+    mkdir('nginx', [
+      mkfile('nginx.conf'),
+    ]),
   ]),
-  mkfile('hexletrc'),
-  mkdir('bin', [
-    mkfile('ls'),
-    mkfile('cat'),
+  mkdir('consul', [
+    mkfile('config.json'),
+    mkfile('file.tmp'),
+    mkdir('data'),
   ]),
-  mkfile('slava'),
+  mkfile('hosts'),
+  mkfile('resolve'),
 ]);
 
-// В реализации используем рекурсивный процесс,
-// чтобы добраться до самого дна дерева.
-const getNodesCount = (tree) => {
-  if (isFile(tree)) {
-    // Возвращаем 1 для учёта текущего файла
+const getFilesCount = (node) => {
+  if (isFile(node)) {
     return 1;
   }
 
-  // Если узел — директория, получаем его детей
-  const children = getChildren(tree);
-  console.log(children);
-  // Самая сложная часть
-  // Считаем количество потомков для каждого из детей,
-  // вызывая рекурсивно нашу функцию getNodesCount
-  const descendantCounts = children.map((child) => getNodesCount(child));
-  console.log(descendantCounts);
-  console.log(1 + _.sum(descendantCounts));
-  // Возвращаем 1 (текущая директория) + общее количество потомков
-  return 1 + _.sum(descendantCounts);
+  const children = getChildren(node);
+  const descendantCounts = children.map(getFilesCount);
+  return _.sum(descendantCounts);
 };
 
-getNodesCount(tree);
+const getSubdirectoriesInfo = (tree) => {
+  const children = getChildren(tree);
+  const result = children
+    // Нас интересуют только директории
+    .filter(isDirectory)
+    // Запускаем подсчёт для каждой директории
+    .map((child) => [getName(child), getFilesCount(child)]);
+
+    console.log(children);
+
+  return result;
+};
+
+console.log(getSubdirectoriesInfo(tree));
